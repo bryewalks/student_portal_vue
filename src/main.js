583cfd1c07b589de6605b2e4915fc9fae9113c88
bryewalks <bryewalks@gmail.com>
2019-03-07 11:00:24 -0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+
+axios.defaults.baseURL = 
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false
 
