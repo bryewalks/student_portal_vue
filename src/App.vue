f5c994ded1077b91fd92df4b78badc6e62a2b79f
markhilkert <mark.hilkert@gmail.com>
2019-03-08 15:30:29 -0600

    changed routes so / takes you to login if not logged in, or your show page if you are

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <router-link class="navbar-brand" to="/studentsportal">Student Portal</router-link>
+      <router-link class="navbar-brand" to="/">Student Portal</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <router-link class="nav-link" to="/studentsportal">Home <span class="sr-only">(current)</span></router-link>
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
           </li>
           
           
@@ -20,8 +20,8 @@
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
-          <router-link class="dropdown-item" to="/studentsportal">Student Portal</router-link>
-          <router-link class="dropdown-item" to="/">Login</router-link>
+          <router-link class="dropdown-item" to="/">Student Portal</router-link>
+          <router-link class="dropdown-item" to="/login">Login</router-link>
           <router-link class="dropdown-item" to="/logout">Logout</router-link>
 
         </div>
