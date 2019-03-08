83fe92b92037f9a4353396869af59ea5a3c74de1
bryewalks <bryewalks@gmail.com>
2019-03-08 10:19:08 -0600

    new buttons

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import axios from "axios";
 
 axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+  process.env.NODE_ENV === "development" ? "https://kill-nil.herokuapp.com" : "/";
 
 Vue.config.productionTip = false
 
