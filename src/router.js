0a0854682718020962cb3881f606469f7a64b20c
farfalle211 <farfalle211@gmail.com>
2019-03-08 10:57:04 -0600

    edited skills edit page and added skills new page, updated router

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import StudentsShow from './views/StudentsShow.vue'
 import StudentsEdit from './views/StudentsEdit.vue'
 import ExperiencesEdit from './views/ExperiencesEdit.vue'
 import SkillsEdit from './views/SkillsEdit.vue'
+import SkillsNew from './views/SkillsNew.vue'
 import EducationsEdit from './views/EducationsEdit.vue'
 import ExperiencesNew from './views/ExperiencesNew.vue'
 import EducationsNew from './views/EducationsNew.vue'
@@ -21,8 +22,8 @@ export default new Router({
     { path: '/logout', name: 'logout', component: Logout },
     { path: '/students/:id', name: 'students-show', component: StudentsShow },
     { path: '/students/:id/edit', name: 'students-edit', component: StudentsEdit },
-
     { path: '/skills/:id/edit', name: 'skills-edit', component: SkillsEdit },
+    { path: '/skills/new', name: 'skills-new', component: SkillsNew },
     { path: '/experiences/new', name: 'experiences-new', component: ExperiencesNew },
     { path: '/experiences/:id/edit', name: 'experiences-edit', component: ExperiencesEdit },
     { path: '/educations/new', name: 'educations-new', component: EducationsNew },
